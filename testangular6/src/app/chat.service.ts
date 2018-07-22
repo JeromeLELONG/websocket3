63f85f3967795c4bf8a88c345c32f7bcfc906b4e
JeromeLELONG <lelong@cnam.fr>
2018-07-22 07:52:59 -0700

    MESSAGE DESCRIPTIF COURT DES MODIFICATIONS

--- testangular6/src/app/chat.service.ts
+++ testangular6/src/app/chat.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs/Rx';
 import { WebsocketService } from './websocket.service';
 
-const CHAT_URL = 'ws://localhost:3000/ws2/';
+const CHAT_URL = 'ws://localhost/ws2/';
 
 export interface Message {
 	author: string,
